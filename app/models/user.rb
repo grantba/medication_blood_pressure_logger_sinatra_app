1e41a458cb78f6dfa65f9a9732c02ad274bde195
Blaire <grant.blaire@gmail.com>
2021-02-24 17:20:57 -0600

    Add find_by_matched_slug method to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ActiveRecord::Base
     has_many :medications
     has_many :physicians
     has_many :pharmacies
+
+    def self.find_by_matched_slug(slug)
+        User.all.find {|user| user.username.downcase == slug.downcase}
+    end
+    
 end
