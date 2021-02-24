062bea31e96fdca785b65defa1ded310358adc43
Blaire <grant.blaire@gmail.com>
2021-02-24 16:58:58 -0600

    Set up config/environment file

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,10 @@
-ENV['SINATRA_ENV'] ||= "development"
-
 require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
+Bundler.require
+
+require_all './app'
 
 ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  adapter: "sqlite3",
+  database: "db/meds_bps_log.sqlite"
 )
 
-require './app/controllers/application_controller'
-require_all 'app'
