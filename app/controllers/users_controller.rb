020ce4e4af455bb0179e1c772f735d2f5ee7dde6
Blaire <grant.blaire@gmail.com>
2021-02-24 18:52:38 -0600

    Add get edit and patch routes to users_controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,4 +18,27 @@ class UsersController < ApplicationController
         end
     end
 
+    get "/users/:slug/edit" do
+        if logged_in?
+            @user = User.find_by_matched_slug(params[:slug])
+            erb :"/users/edit"
+        else
+            flash[:error] = "You are not currently logged in to your account. Please log in or sign up if you have not created an account yet."
+                redirect to "/"
+            end
+    end
+
+    patch "/users/:slug" do
+        @user = User.find_by_matched_slug(params[:slug])
+        @user.name = params[:name] unless params[:name].blank?
+        @user.date_of_birth = params[:date_of_birth] unless params[:date_of_birth].blank?
+        @user.address = params[:address] unless params[:address].blank?
+        @user.phone_number = params[:phone_number] unless params[:phone_number].blank?
+        @user.email = params[:email] unless params[:email].blank?
+        @user.username = params[:username] unless params[:username].blank?
+        @user.password = params[:password] unless params[:password].blank?
+        @user.save 
+        flash[:message] = "Your information has been updated successfully!"
+        redirect to "/users/#{@user.username}"
+    end
 end
