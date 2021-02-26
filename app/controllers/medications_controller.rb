ee542b7ae9284f67e8105d8385f8ce79beb19ffb
Blaire <grant.blaire@gmail.com>
2021-02-25 18:45:23 -0600

    Refactored routes in medications_controller to accomodate both users medications and family members medications.

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -49,21 +49,28 @@ class MedicationsController < ApplicationController
         end
     end
 
-    post "/medications" do
+    post "/medications/users" do
         @med = Medication.new(params)
         @med.user_id = @current_user.id 
         @med.save
-        redirect to "/medications/#{@med.id}"
+        redirect to "/medications/users/#{@med.id}"
     end
 
-    get "/medications/:id/edit" do
+    post "/medications/familymembers" do
+        @med = Medication.new(params)
+        @med.family_member_id = FamilyMember.find_by(id: params[:id])
+        @med.save
+        redirect to "/medications/familymembers/#{@med.id}"
+    end
+
+    get "/medications/users/:id/edit" do
         if logged_in?
             @med = Medication.find_by(id: params[:id])
-            if @med.user_id == @current_user.id || @current_user.family_members.include?(@med.family_member_id)
-                erb :"/medications/edit"
+            if @med.user_id == @current_user.id
+                erb :"/medications/users/edit"
             else
                 flash[:alert] = "You can only edit the medications that belong to either you or your family members."
-                redirect to "/medications"
+                redirect to "/medications/users"
             end
         else
             flash[:alert] = "You must be logged in to edit any of your medications."
@@ -71,22 +78,48 @@ class MedicationsController < ApplicationController
         end       
     end
 
-    patch "/medications/:id" do
+    get "/medications/familymembers/:id/edit" do
+        if logged_in?
+            @med = Medication.find_by(id: params[:id])
+            if @current_user.family_members.include?(@med.family_member_id)
+                erb :"/medications/familymembers/edit"
+            else
+                flash[:alert] = "You can only edit the medications that belong to either you or your family members."
+                redirect to "/medications/familymembers"
+            end
+        else
+            flash[:alert] = "You must be logged in to edit any of your medications."
+            redirect to "/login"
+        end       
+    end
+
+    patch "/medications/users/:id" do
         @med = Medication.find_by(id: params[:id])
-        if @med.user_id == @current_user.id || @current_user.family_members.include?(@med.family_member_id)
+        if @med.user_id == @current_user.id 
             @med.update(name: params[:name], dose: params[:dose], form: params[:form], route: params[:route], time_to_be_taken: params[:time_to_be_taken], category: [:category], prescription_number: params[:prescription_number], date_filled: params[:date_filled], refill_date: params[:refill_date], discard_or_expiration_date: params[:discard_or_expiration_date], side_effects: params[:side_effects], notes: params[:notes]) 
-            redirect to "/medications/#{@med.id}"
+            redirect to "/medications/users/#{@med.id}"
         else
             flash[:alert] = "You can only edit the medications that belong to either you or your family members."
-            redirect to "/medications"
+            redirect to "/medications/users"
         end
     end
 
-    get "/medications/:id" do
+    patch "/medications/family/members/:id" do
+        @med = Medication.find_by(id: params[:id])
+        if @current_user.family_members.include?(@med.family_member_id)
+            @med.update(name: params[:name], dose: params[:dose], form: params[:form], route: params[:route], time_to_be_taken: params[:time_to_be_taken], category: [:category], prescription_number: params[:prescription_number], date_filled: params[:date_filled], refill_date: params[:refill_date], discard_or_expiration_date: params[:discard_or_expiration_date], side_effects: params[:side_effects], notes: params[:notes]) 
+            redirect to "/medications/familymembers/#{@med.id}"
+        else
+            flash[:alert] = "You can only edit the medications that belong to either you or your family members."
+            redirect to "/medications/familymembers"
+        end
+    end
+
+    get "/medications/users/:id" do
         if logged_in?
             @med = Medication.find_by(id: params[:id])
             if @med.user_id == @current_user.id 
-                erb :"/medications/show"
+                erb :"/medications/users/show"
             else
                 flash[:alert] = "You can only view yours or your family member's medications."
                 redirect to "/users/hompage"
@@ -97,12 +130,44 @@ class MedicationsController < ApplicationController
         end
     end
 
-    delete "/medications/:id" do
+    get "/medications/familymembers/:id" do
         if logged_in?
-            if @med.user_id == @current_user.id || @current_user.family_members.include?(@med.family_member_id)
-                @med = Medication.find(params[:id])
+            @med = Medication.find_by(id: params[:id])
+            if @current_user.family_members.include?(@med.family_member_id)
+                # or if @current_user.family_members.medications.include?(@med)
+                erb :"/medications/familymembers/show"
+            else
+                flash[:alert] = "You can only view yours or your family member's medications."
+                redirect to "/users/hompage"
+            end
+        else
+            flash[:alert] = "You must be logged in to view your medications."
+            redirect to "/login"
+        end
+    end
+
+    delete "/medications/users/:id" do
+        if logged_in?
+            @med = Medication.find_by(id: params[:id])
+            if @med.user_id == @current_user.id 
                 @med.destroy
-                redirect to "/medications"
+                redirect to "/medications/users"
+            else
+                flash[:alert] = "You can only delete yours or your family member's medications."
+                redirect to "/users/hompage"
+            end
+        else
+            flash[:alert] = "You must be logged in to delete any of your medications."
+            redirect to "/login"
+        end
+    end
+
+    delete "/medications/familymembers/:id" do
+        if logged_in?
+            @med = Medication.find_by(id: params[:id])
+            if @current_user.family_members.include?(@med.family_member_id)
+                @med.destroy
+                redirect to "/medications/familymembers"
             else
                 flash[:alert] = "You can only delete yours or your family member's medications."
                 redirect to "/users/hompage"
