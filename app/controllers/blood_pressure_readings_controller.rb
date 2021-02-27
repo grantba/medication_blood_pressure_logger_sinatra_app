e335515b0d8f0143bbd417c65ed1128922413748
Blaire <grant.blaire@gmail.com>
2021-02-27 02:06:19 -0600

    Debugged and refactored code in blood_pressure_readings_controller. Completed bloodpressurereadings/edit.erb file, bloodpressurereadings/index.erb file, bloodpressurereadings/new.erb file, and bloodpressurereadings/show.erb file.

--- app/controllers/blood_pressure_readings_controller.rb
+++ app/controllers/blood_pressure_readings_controller.rb
@@ -1,14 +1,13 @@
 class BloodPressureReadingsController < ApplicationController
 
-    get "/bloodpressurereadings/users/:slug" do
+    get "/bloodpressurereadings" do 
         if logged_in?
-            @user = User.find_by_matched_slug(params[:slug])
-            if @user.id == @current_user.id 
-                @bps = @current_user.bloodpressurereadings
-                erb :"/bloodpressurereadings/users/index"
+            if current_user
+                @bps = current_user.blood_pressure_readings
+                erb :"/bloodpressurereadings/index"
             else
-                flash[:alert] = "You can only view yours or your family member's blood pressure readings."
-                redirect to "/users/#{@current_user.username}"
+                flash[:alert] = "You can only view your own blood pressure readings."
+                redirect to "/users/#{current_user.username}"
             end
         else
             flash[:alert] = "You must be logged in to view your blood pressure readings."
@@ -16,14 +15,14 @@ class BloodPressureReadingsController < ApplicationController
         end
     end
 
-    get "/bloodpressurereadings/users/:slug/new" do
+    get "/bloodpressurereadings/:slug/new" do
         @user = User.find_by_matched_slug(params[:slug])
         if logged_in?
-            if @user.id == @current_user.id 
-                erb :"/bloodpressurereadings/users/new"
+            if @user.id == current_user.id 
+                erb :"/bloodpressurereadings/new"
             else
-                flash[:alert] = "You can only add new data to yours or your  member's blood pressure readings log."
-                redirect to "/users/#{@current_user.username}"
+                flash[:alert] = "You can only add a new blood pressure reading to your own blood pressure readings log."
+                redirect to "/bloodpressurereadings"
             end
         else
             flash[:alert] = "You must be logged in to submit a new blood pressure reading."
@@ -31,27 +30,31 @@ class BloodPressureReadingsController < ApplicationController
         end
     end
 
-    post "/bloodpressurereadings/users/:slug" do
-        @user = User.find_by_matched_slug(params[:slug])
-        if @user.id == @current_user.id
-            @bp = BloodPressureReading.new(params)
-            @bp.user_id = @current_user.id 
-            @bp.save
-            redirect to "/bloodpressurereadings/users/#{@bp.id}"
+    post "/bloodpressurereadings" do
+        if logged_in?
+            if current_user
+                @bp = BloodPressureReading.new(params)
+                @bp.user_id = current_user.id 
+                @bp.save
+                redirect to "/bloodpressurereadings/#{@bp.id}"
+            else
+                flash[:alert] = "You can only add a new blood pressure reading to your own blood pressure reading log."
+                redirect to "/bloodpressurereadings"
+            end
         else
-            flash[:alert] = "You can only add new data to yours or your  member's blood pressure readings log."
-            redirect to "/users/#{@current_user.username}"
+            flash[:alert] = "You must be logged in to submit a new blood pressure reading."
+            redirect to "/login"
         end
     end
 
-    get "/bloodpressurereadings/users/:id/edit" do
+    get "/bloodpressurereadings/:id/edit" do
         if logged_in?
             @bp = BloodPressureReading.find_by(id: params[:id])
-            if @bp.user_id == @current_user.id
-                erb :"/bloodpressurereadings/users/edit"
+            if @bp.user_id == current_user.id
+                erb :"/bloodpressurereadings/edit"
             else
-                flash[:alert] = "You can only edit the blood pressure readings that belong to either you or your family members."
-                redirect to "/bloodpressurereadings/users"
+                flash[:alert] = "You can only edit the blood pressure readings that belong to you."
+                redirect to "/bloodpressurereadings"
             end
         else
             flash[:alert] = "You must be logged in to edit any of your blood pressure readings."
@@ -59,25 +62,28 @@ class BloodPressureReadingsController < ApplicationController
         end       
     end
 
-    patch "/bloodpressurereadings/users/:id" do
-        @bp = BloodPressureReading.find_by(id: params[:id])
-        if @bp.user_id == @current_user.id 
-            @bp.update(name: params[:name], dose: params[:dose], form: params[:form], route: params[:route], time_to_be_taken: params[:time_to_be_taken], category: [:category], prescription_number: params[:prescription_number], date_filled: params[:date_filled], refill_date: params[:refill_date], discard_or_expiration_date: params[:discard_or_expiration_date], side_effects: params[:side_effects], notes: params[:notes]) 
-            redirect to "/bloodpressurereadings/users/#{@bp.id}"
+    patch "/bloodpressurereadings/:id" do
+        bp = BloodPressureReading.find_by(id: params[:id])
+        if bp.user_id == current_user.id 
+            bp.notes = params[:notes] unless params[:notes].blank?
+            bp.save 
+            bp.update(date: params[:date], time: params[:time], systolic_reading: params[:systolic_reading], diastolic_reading: params[:diastolic_reading], pulse: params[:pulse], device_used: params[:device_used], site_used: params[:site_used]) 
+            flash[:message] = "Your blood pressure reading information has been updated successfully!"
+            redirect to "/bloodpressurereadings/#{bp.id}"
         else
-            flash[:alert] = "You can only edit the blood pressure readings that belong to either you or your family members."
-            redirect to "/bloodpressurereadings/users"
+            flash[:alert] = "You can only edit the blood pressure readings that belong to you."
+            redirect to "/bloodpressurereadings"
         end
     end
 
-    get "/bloodpressurereadings/users/:id" do
+    get "/bloodpressurereadings/:id" do
         if logged_in?
             @bp = BloodPressureReading.find_by(id: params[:id])
-            if @bp.user_id == @current_user.id 
-                erb :"/bloodpressurereadings/users/show"
+            if @bp.user_id == current_user.id 
+                erb :"/bloodpressurereadings/show"
             else
-                flash[:alert] = "You can only view yours or your family member's blood pressure readings."
-                redirect to "/users/hompage"
+                flash[:alert] = "You can only view your own blood pressure readings."
+                redirect to "/users/#{current_user.username}"
             end
         else
             flash[:alert] = "You must be logged in to view your blood pressure readings."
@@ -85,15 +91,15 @@ class BloodPressureReadingsController < ApplicationController
         end
     end
 
-    delete "/bloodpressurereadings/users/:id" do
+    delete "/bloodpressurereadings/:id" do
         if logged_in?
             @bp = BloodPressureReading.find_by(id: params[:id])
-            if @bp.user_id == @current_user.id 
+            if @bp.user_id == current_user.id 
                 @bp.destroy
-                redirect to "/bloodpressurereadings/users"
+                redirect to "/bloodpressurereadings"
             else
-                flash[:alert] = "You can only delete yours or your family member's blood pressure readings."
-                redirect to "/users/hompage"
+                flash[:alert] = "You can only delete your own blood pressure readings."
+                redirect to "/bloodpressurereadings"
             end
         else
             flash[:alert] = "You must be logged in to delete any of your blood pressure readings."
